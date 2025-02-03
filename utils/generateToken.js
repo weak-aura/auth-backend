67a17763af28996f9fc97ec1e22f56a825e6e04a
Bekzat <developer.bekzat@gmail.com>
2025-02-03 23:39:54 +0600

    secure

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const generateToken = (id, res) => {
   const token = jwt.sign({id}, process.env.TOKEN_SECRET, {expiresIn: "1h"})
-  res.cookie("token", token, {maxAge: 3600000, httpOnly: true, sameSite: "none"})
+  res.cookie("token", token, {maxAge: 3600000, httpOnly: true, sameSite: "none", secure: true})
 }
 
 module.exports = {generateToken}
